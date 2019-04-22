41a6922e73855eaf29680e04c6b14a5bfa338a4d
Piotr Zientara <314zientara@gmail.com>
2019-04-22 14:39:02 +0200

    Add runner for multiple profiles

---
+++ run_rds_script_for_multiple_profiles.zsh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+## these array contains profiles defined in .aws/config and .aws/credentials
+declare -a profiles=("default" "appnroll")
+
+DATABASE="aws_instances"
+TABLE_NAME="aws_rds"
+
+PREVIOUS_PROFILE=$AWS_PROFILE
+for aws_profile in "${profiles[@]}"
+do
+   echo -e "\nSaving AWS RDS Instances from aws profile:'$aws_profile'..."
+   export AWS_PROFILE=$aws_profile
+   zsh aws_rds_instances_from_all_regions_to_db.zsh $DATABASE $TABLE_NAME
+done
+
+
+## return to previous profile
+export AWS_PROFILE=$PREVIOUS_PROFILE
+
+psql -c "COPY $TABLE_NAME TO '$HOME/$TABLE_NAME.csv' DELIMITER ',' CSV HEADER;" -d $DATABASE
+cd $HOME
+open "$TABLE_NAME.csv"
