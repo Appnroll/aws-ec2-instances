51741f7d77d528028c29ee4b3d54c5400a904181
Piotr Zientara <314zientara@gmail.com>
2019-04-22 14:32:34 +0200

    Add script for rds databases

---
+++ sql/aws_rds_instances.sql
@@ -0,0 +1,39 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.1
+-- Dumped by pg_dump version 11.1
+-- run:
+-- psql aws_instances -f sql/aws_rds_instances.sql
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+
+CREATE TABLE public.aws_rds (
+    id serial PRIMARY KEY,
+    dbi_resource_id VARCHAR (32) UNIQUE,
+    instance_class VARCHAR (32),
+    identifier VARCHAR (32)
+);
+
+
+COPY public.aws_rds (dbi_resource_id, instance_class, identifier) FROM stdin;
+\.
+
+--
+-- PostgreSQL database dump complete
+--
+
